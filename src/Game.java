3633d39098ab2873f3982eb826ff2b4ce8e046e3
starlightglim <195524079+starlightglim@users.noreply.github.com>
2025-01-25 22:45:54 -0500

    added paddle

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game extends JPanel {
-    // Game variables will go here later
+    // paddle properties
+    int paddleX=350; //start xpos
+    int paddleY=500; //start ypos
+    int paddleW=100; //width
+    int paddleH=15; //height
 
     public static void main(String[] args) {
        JFrame frame = new JFrame("Brick Breaker");
        Game game = new Game();
        frame.add(game);
-       frame.setsize(800, 600); //window size
+       frame.setSize(800, 600); //window size
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setVisible(true);
     }
 }
